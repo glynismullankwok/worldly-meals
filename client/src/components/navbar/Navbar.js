e1595a7ef3971b7fccc34e57a5a20b82cad60df1
Glynis Mullan-Kwok <glynismullankwok@gmail.com>
2021-04-13 22:55:57 -0400

    home page, navbar

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,37 +1,37 @@
-import React from 'react'
+// import React from 'react'
 
-const Navbar = () => {
-    return (
-        <div style={{border: '1px solid lightgray', width:'100vw',height:'70px', backgroundImage: 'linear-gradient(120deg, #a1c4fd 0%, #c2e9fb 100%)'}}>
-            <ul style={ { display:'flex',flexDirection: 'row', justifyContent: 'space-around'} }>
-                <li style={{textDecoration:'none'}}>
-                    <a href="/">Home</a>
-                </li >
-                <li style={{textDecoration:'none'}}>
-                    <a href="/signup">Signup</a>
-                </li >
-                <li style={{textDecoration:'none'}}>
-                    <a href="/login">login</a>
-                </li >
-                <li style={{textDecoration:'none'}}>
-                    <a href="/recipe">Recipe</a>
-                </li >
-                <li style={{textDecoration:'none'}}>
-                    <a href="/item">Item</a>
-                </li >
-                <li style={{textDecoration:'none'}}>
-                    <a href="/ingredient">Ingredient</a>
-                </li >
-                <li style={{textDecoration:'none'}}>
-                    <a href="/digest">Digest</a>
-                </li >
-                <li style={{textDecoration:'none'}}>
-                    <a href="/foodDatabase">FoodDatabase</a>
-                </li >
+// const Navbar = () => {
+//     return (
+//         <div style={{border: '1px solid lightgray', width:'100vw',height:'70px', backgroundImage: 'linear-gradient(120deg, #a1c4fd 0%, #c2e9fb 100%)'}}>
+//             <ul style={ { display:'flex',flexDirection: 'row', justifyContent: 'space-around'} }>
+//                 <li style={{textDecoration:'none'}}>
+//                     <a href="/">Home</a>
+//                 </li >
+//                 <li style={{textDecoration:'none'}}>
+//                     <a href="/signup">Signup</a>
+//                 </li >
+//                 <li style={{textDecoration:'none'}}>
+//                     <a href="/login">login</a>
+//                 </li >
+//                 <li style={{textDecoration:'none'}}>
+//                     <a href="/recipe">Recipe</a>
+//                 </li >
+//                 <li style={{textDecoration:'none'}}>
+//                     <a href="/item">Item</a>
+//                 </li >
+//                 <li style={{textDecoration:'none'}}>
+//                     <a href="/ingredient">Ingredient</a>
+//                 </li >
+//                 <li style={{textDecoration:'none'}}>
+//                     <a href="/digest">Digest</a>
+//                 </li >
+//                 <li style={{textDecoration:'none'}}>
+//                     <a href="/foodDatabase">FoodDatabase</a>
+//                 </li >
 
-            </ul>
-        </div>
-    )
-}
+//             </ul>
+//         </div>
+//     )
+// }
 
-export default Navbar
+// export default Navbar
