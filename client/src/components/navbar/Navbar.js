f8ccb5d6cdaecfc949a472459ad602717566eedd
Tracy Golden <tracy80s2003@gmail.com>
2021-04-16 12:08:27 -0500

    updating navbar media query, coloring and signup margin. also updated btn color to green

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -26,48 +26,46 @@ function Navbar() {
     window.addEventListener('resize', showButton);
 
     return (
-       
-            <nav className='navbar'>
-                <div className='navbar-container'>
-                    <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
-                        International Ingredients to Worldly Meals
-                        <i className="fas fa-utensils"></i>
-                    </Link>
+        <nav className='navbar'>
+            <div className='navbar-container'>
+                <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+                    International Ingredients to Worldly Meals
+                    {/* <i className="fas fa-utensils"></i> */}
+                </Link>
 
-                    <div className='menu-icon' onClick={handleClick}>
-                        <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
-                    </div>
-
-                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-                        <li className='nav-item'>
-                            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
-                                Home
-                            </Link>
-                        </li>
-
-                        <li className='nav-item'>
-                            <Link to='/recipe' className='nav-links' onClick={closeMobileMenu}>
-                                Recipes
-                            </Link>
-                        </li>
-
-                        <li className='nav-item'>
-                            <Link to='/nutrition' className='nav-links' onClick={closeMobileMenu}>
-                                Nutrition
-                            </Link>
-                        </li>
-
-                        <li className='nav-item'>
-                            <Link to='/signup' className='nav-links-mobile' onClick={closeMobileMenu}>
-                                Sign Up
-                            </Link>
-                        </li>
-                        
-                    </ul>
-                    {button && <Button buttonStyle='btn--outline'>Sign up</Button>}
+                <div className='menu-icon' onClick={handleClick}>
+                    <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
                 </div>
-            </nav>
-       
+
+                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                    <li className='nav-item'>
+                        <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                            Home
+                        </Link>
+                    </li>
+
+                    <li className='nav-item'>
+                        <Link to='/recipe' className='nav-links' onClick={closeMobileMenu}>
+                            Recipes
+                        </Link>
+                    </li>
+
+                    <li className='nav-item'>
+                        <Link to='/nutrition' className='nav-links' onClick={closeMobileMenu}>
+                            Nutrition
+                        </Link>
+                    </li>
+
+                    <li className='nav-item'>
+                        <Link to='/signup' className='nav-links-mobile' onClick={closeMobileMenu}>
+                            Sign Up
+                        </Link>
+                    </li>
+                        
+                </ul>
+                {button && <Button buttonStyle='btn--outline'>Sign up</Button>}
+            </div>
+        </nav>     
     );
 }
 
