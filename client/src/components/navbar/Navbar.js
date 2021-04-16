88e37241678bfb95c11d2eab183eabfb1a963eb6
Tracy Golden <tracy80s2003@gmail.com>
2021-04-15 21:19:42 -0500

    updated searchfood.js

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
-import { Button } from './Button';
+import { Button } from '../Button';
 
 function Navbar() {
     const [click, setClick] = useState(false);
