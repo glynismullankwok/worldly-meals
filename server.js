d15ebe9e4002cb51d349bf86e5092694b6c9d3df
desta mulu <destish21@yahoo.com>
2021-04-17 02:29:27 -0500

    make it better

--- server.js
+++ server.js
@@ -45,8 +45,6 @@ app.get('*', (req, res) => {
 })
 const uri = process.env.MONGODB_URL
 // || "mongodb://localhost/nutrints"
-// || "mongodb://localhost/nutrints"
-
 mongoose.connect(uri,
   {
     useNewUrlParser: true,
