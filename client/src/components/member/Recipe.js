8516addfd9eb6c57598076cb6e3dfbf4b06029a6
desta mulu <destish21@yahoo.com>
2021-04-17 21:51:31 -0500

    Updated api keys

--- client/src/components/member/Recipe.js
+++ client/src/components/member/Recipe.js
@@ -17,12 +17,15 @@ const Recipe = () => {
   }, [search]);
 
   const fetchFood = () => {
-    const APP_ID = process.env.REACT_APP_APP_ID;
-    const API_KEY = process.env.REACT_APP_API_KEY;
+    const REACT_APP_APP_ID="a7e4cdd9"
+    const REACT_APP_API_KEY="efd018e424ec85db972aafa8104f3600"
+
+    // const APP_ID = process.env.REACT_APP_APP_ID;
+    // const API_KEY = process.env.REACT_APP_API_KEY;
     
     axios
       .get(
-        `https://api.edamam.com/search?q=${search}&app_id=${APP_ID}&app_key=${API_KEY}`
+        `https://api.edamam.com/search?q=${search}&app_id=${REACT_APP_APP_ID}&app_key=${REACT_APP_API_KEY}`
       )
       .then(res => {
         // console.log(res.data.hits);
