902ed9920dd6b1542e6d44cec1601caa4f480a94
desta mulu <destish21@yahoo.com>
2021-04-18 22:59:33 -0500

    Updated style.css utils routes and  sever.js

--- server.js
+++ server.js
@@ -10,14 +10,14 @@ const mongoose = require('mongoose');
 const passport = require('./passport');
 
 const apiRoutes = require('./routes/api-route');
-const app = express()
-require('dotenv').config()
+const app = express();
+require('dotenv').config();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json())
+app.use(express.json());
 // app.use(cors());
 
 
@@ -33,8 +33,8 @@ app.use(
 );
 
 // passport middleware here
-app.use(passport.initialize())
-app.use(passport.session())
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Serve up static assets(usually on heroku)
 if (process.env.NODE_ENV === 'production') {
@@ -42,7 +42,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 // Define API routes here
-app.use(apiRoutes)
+app.use(apiRoutes);
 
 // Send every other request to the React app 
 // Define any API routes before this runs
@@ -50,8 +50,8 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './client/build/index.html'));
 
 })
-const uri = process.env.MONGODB_URL
-|| "mongodb://localhost/nutrints"
+const uri = process.env.MONGODB_URL;
+// || "mongodb://localhost/nutrints"
 mongoose.connect(uri,
   {
     useNewUrlParser: true,
